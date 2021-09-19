9c9cd3af36f957800865e512da14624d45307041
wxy <wxy@qq.com>
2021-09-19 17:40:45 +0800

    push commit

--- src/main/java/com/wxy/git/GitTest.java
+++ src/main/java/com/wxy/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello Git4!");
         System.out.println("Git test!");
         System.out.println("Hello Git5!");
+        System.out.println("Push Git5!");
     }
 }
